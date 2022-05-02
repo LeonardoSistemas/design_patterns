a0c4dc1e5a3b22a3a3fd64f7619330bdb4f70530
Leonardo Treviso <leonardosistemas@outlook.com>
2022-05-01 22:45:43 -0300

    feat: criando interfaces de abstract product

---
+++ src/AbstractFactory/AbstractProductB.ts
@@ -0,0 +1,7 @@
+import AbstractProductA from "./AbstractProductA";
+
+export default interface AbstractProductB {
+    usefulFunctionB(): string;
+
+    anotherUsefulFunctionB(collaborator: AbstractProductA): string;
+}
