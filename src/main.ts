16ba3667da701a73af6d972809a2b57205376e28
Leonardo Treviso <leonardosistemas@outlook.com>
2022-05-01 22:47:03 -0300

    feat: criando a chamada para o padrão AbstractFactory

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,5 @@
+import AbstractFactory from "./AbstractFactory/AbstractFactory";
+import ConcreteFactory1 from "./AbstractFactory/ConcreteFactory1";
 import ConcreteCreator1 from "./FactoryMethod/ConcreteCreator1";
 import Creator from "./FactoryMethod/Creator";
 
@@ -6,4 +8,12 @@ function clientCode(creator: Creator) {
     console.log(creator.someOperation());
 }
 
+function clientAbstractFactory(factory: AbstractFactory) {
+    const productA = factory.createProductA();
+    const productB = factory.createProductB();
+    console.log(productB.usefulFunctionB());
+    console.log(productB.anotherUsefulFunctionB(productA));
+}
+
 clientCode(new ConcreteCreator1());
+clientAbstractFactory(new ConcreteFactory1());
