3c1c170a190656360a574445c47b8b28494ea78e
Leonardo Treviso <leonardosistemas@outlook.com>
2022-05-12 23:56:16 -0300

    chore: chamando implementação do prototype na main

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import AbstractFactory from "./AbstractFactory/AbstractFactory";
 import ConcreteFactory1 from "./AbstractFactory/ConcreteFactory1";
 import Director from "./Builder/Director";
 import ConcreteBuilder from "./Builder/ConcreteBuilder1";
+import Prototype from "./Prototype/Prototype";
+import ComponetWithBackReference from "./Prototype/ComponentWithBackReference";
 
 function clientCode(creator: Creator) {
     console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
@@ -17,7 +19,7 @@ function clientAbstractFactory(factory: AbstractFactory) {
     console.log(productB.anotherUsefulFunctionB(productA));
 }
 
-function clientBuilder(director: Director){
+function clientBuilder(director: Director) {
     const builder = new ConcreteBuilder();
     director.setBuilder(builder);
     console.log('Standard full featured product');
@@ -29,6 +31,39 @@ function clientBuilder(director: Director){
     builder.getProduct().listParts();
 }
 
+function clientPrototype() {
+    const p1 = new Prototype();
+    p1.primitive = 245;
+    p1.component = new Date();
+    p1.circularReference = new ComponetWithBackReference(p1);
+
+    const p2 = p1.clone();
+    if (p1.primitive === p2.primitive) {
+        console.log('Primitive field values have been carried over to a clone. Yay!');
+    } else {
+        console.log('Primitive field values have not been copied. Booo!');
+    }
+    if (p1.component === p2.component) {
+        console.log('Simple component has not been cloned. Booo!');
+    } else {
+        console.log('Simple component has been cloned. Yay!');
+    }
+
+    if (p1.circularReference === p2.circularReference) {
+        console.log('Component with back reference has not been cloned. Booo!');
+    } else {
+        console.log('Component with back reference has been cloned. Yay!');
+    }
+
+    if (p1.circularReference.prototype === p2.circularReference.prototype) {
+        console.log('Component with back reference is linked to original object. Booo!');
+    } else {
+        console.log('Component with back reference is linked to the clone. Yay!');
+    }
+
+}
+
 clientCode(new ConcreteCreator1());
 clientAbstractFactory(new ConcreteFactory1());
 clientBuilder(new Director);
+clientPrototype();
