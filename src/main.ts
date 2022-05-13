2a81d698af7f809ed9896d6031d53c4e4d3411e0
Leonardo Treviso <leonardosistemas@outlook.com>
2022-05-12 22:59:52 -0300

    chore: chamando o padrão builder

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import AbstractFactory from "./AbstractFactory/AbstractFactory";
-import ConcreteFactory1 from "./AbstractFactory/ConcreteFactory1";
 import ConcreteCreator1 from "./FactoryMethod/ConcreteCreator1";
 import Creator from "./FactoryMethod/Creator";
+import AbstractFactory from "./AbstractFactory/AbstractFactory";
+import ConcreteFactory1 from "./AbstractFactory/ConcreteFactory1";
+import Director from "./Builder/Director";
+import ConcreteBuilder from "./Builder/ConcreteBuilder1";
 
 function clientCode(creator: Creator) {
     console.log('Client: I\'m not aware of the creator\'s class, but it still works.');
@@ -15,5 +17,18 @@ function clientAbstractFactory(factory: AbstractFactory) {
     console.log(productB.anotherUsefulFunctionB(productA));
 }
 
+function clientBuilder(director: Director){
+    const builder = new ConcreteBuilder();
+    director.setBuilder(builder);
+    console.log('Standard full featured product');
+    director.buildFullFeatureProduct();
+    builder.getProduct().listParts();
+    console.log('Custom product');
+    builder.producePartA();
+    builder.producePartC();
+    builder.getProduct().listParts();
+}
+
 clientCode(new ConcreteCreator1());
 clientAbstractFactory(new ConcreteFactory1());
+clientBuilder(new Director);
