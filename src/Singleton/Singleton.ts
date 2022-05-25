e7e58507ab9905dd9726b767783e0aa3d17779ef
Leonardo Treviso <leonardosistemas@outlook.com>
2022-05-24 23:13:17 -0300

    feat: criando padrão singleton

---
+++ src/Singleton/Singleton.ts
@@ -0,0 +1,17 @@
+export default class Singleton {
+    private static intance : Singleton;
+
+    private constructor(){}
+
+    public static getInstance() : Singleton {
+        if(!Singleton.intance) {
+            Singleton.intance = new Singleton();
+        }
+
+        return Singleton.intance;
+    }
+
+    public someBusinessLogic(){
+        
+    }
+}
